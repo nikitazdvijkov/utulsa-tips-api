b73fe9d28b028355ffa3dc439d296ca429007c74
Nikita Zdvijkov <nikitazdvijkov@outlook.com>
2020-02-15 19:16:53 -0600

    hopefully finalized tip schema

--- api/models/tip.js
+++ api/models/tip.js
@@ -1,14 +1,7 @@
-// this file added in first vid on database
-// mongodb works on schemas and models, this js file defines that
-// defs: model is constructor that builds objects based on schema, which is layout
-
 const mongoose = require('mongoose');
 
-const productSchema = mongoose.Schema({
-    // notice same string without Schema in other products.js file
-    // ie mongoose.Types.ObjectId
-    // mindfuck
-    _id: mongoose.Schema.Types.ObjectId, // underscore by convention
+const tipSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
     alias: String,
     body: String,
     timestamp: Date,
@@ -16,7 +9,4 @@ const productSchema = mongoose.Schema({
     live: Boolean
 });
 
-// 2 args:
-// first: name of model as you want to use it internally (title case by convention)
-// second arg: schema to use for model
 module.exports = mongoose.model('Tip', tipSchema);
